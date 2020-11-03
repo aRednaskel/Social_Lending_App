6757bdc1dea41d678646dc2c84322fbaca8d05cd
Michał Nielipiński <bukaniex@gmail.com>
2020-11-03 08:39:07 +0100

    Added BCryptPasswordEncoder bean configuration to SpringBootApplication

--- src/main/java/pl/fintech/challenge2/backend2/Application.java
+++ src/main/java/pl/fintech/challenge2/backend2/Application.java
@@ -2,12 +2,17 @@ package pl.fintech.challenge2.backend2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Application {
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
