215ac359bf3cdb2518fb9d1784a92ef84b978f1c
Michał Nielipiński <bukaniex@gmail.com>
2020-11-03 16:30:28 +0100

    configured swagger

---
+++ src/main/java/pl/fintech/challenge2/backend2/config/SwaggerConfiguration.java
@@ -0,0 +1,23 @@
+package pl.fintech.challenge2.backend2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("pl.fintech.challenge2.backend2"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
