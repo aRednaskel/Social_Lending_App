fa61158107cf849eff6b7b2ec7b622887358bb1e
Michał Nielipiński <bukaniex@gmail.com>
2020-11-06 17:36:05 +0100

    adding role works

--- src/main/java/pl/fintech/challenge2/backend2/domain/user/RoleRepository.java
+++ src/main/java/pl/fintech/challenge2/backend2/domain/user/RoleRepository.java
@@ -2,5 +2,8 @@ package pl.fintech.challenge2.backend2.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
 }
