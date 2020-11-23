6501e697423cdfc9f47f82b12d60eb10eece21d7
Michał Nielipiński <bukaniex@gmail.com>
2020-11-23 20:57:54 +0100

    for now everybody can access this endpoint

--- src/main/java/pl/fintech/challenge2/backend2/controller/UserController.java
+++ src/main/java/pl/fintech/challenge2/backend2/controller/UserController.java
@@ -30,6 +30,7 @@ public class UserController {
         return ResponseEntity.status(201).body(userService.saveUser(userMapper.mapRegistrationDTOToUser(userDTO)));
     }
 
+    @PreAuthorize("permitAll()")
     @GetMapping("/{id}")
     public ResponseEntity getUsersData(@PathVariable Long id) {
         log.info("GET /api/users/{}", id);
