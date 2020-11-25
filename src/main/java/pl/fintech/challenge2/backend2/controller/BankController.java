a73b3ac77c0b96b5caa4b5689fb0e14f9bcf242a
aleksander.zulewski <azulewski@gmail.com>
2020-11-25 17:53:03 +0100

    Added rest config

--- src/main/java/pl/fintech/challenge2/backend2/controller/BankController.java
+++ src/main/java/pl/fintech/challenge2/backend2/controller/BankController.java
@@ -1,6 +1,7 @@
 package pl.fintech.challenge2.backend2.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.fintech.challenge2.backend2.domain.bank.BankAppClient;
@@ -10,24 +11,27 @@ import pl.fintech.challenge2.backend2.domain.user.UserService;
 import java.math.BigDecimal;
 
 @RestController
-@RequestMapping("/api/bank")
+@RequestMapping("/api/transactions")
 @RequiredArgsConstructor
 public class BankController {
     
     private final BankAppClient bankAppClient;
     private final UserService userService;
 
+    @PostMapping
     public void createInternalTransaction(Long senderId, Long receiverId, BigDecimal value) {
         User sender = userService.findById(senderId);
         User receiver = userService.findById(receiverId);
         bankAppClient.createInternalTransaction(sender, receiver, value);
     };
 
+    @PostMapping("/externalPayment")
     public void createExternalPayment(Long userId, BigDecimal value){
         User user = userService.findById(userId);
         bankAppClient.createExternalPayment(user,value);
     };
 
+    @PostMapping("/externalWithdrawal")
     public void createExternalWithdrawal(Long userId, BigDecimal value){
         User user = userService.findById(userId);
         bankAppClient.createExternalWithdrawal(user,value);
